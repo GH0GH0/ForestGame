382752eae2507414cb8c878ec9366de496a03361
Lylia <lilia.hamidi122@gmail.com>
2019-01-01 21:36:09 +0100

    Methodes personnage.cpp

--- personnage.cpp
+++ personnage.cpp
@@ -1,4 +1,5 @@
 #include "personnage.h"
+#include "forest.h"
 
 Personnage::Personnage()
 {
@@ -18,10 +19,16 @@ Personnage::Personnage(int x, int y, int d, int vie)
 
 Personnage::Personnage(Personnage const & tocopy)
 {
+	this->p_x=tocopy.getX();
+	this->p_y=tocopy.getY();
+	this->p_d=tocopy.getD();
+	this->p_vie=tocopy.getVie();
 }
+
 
 Personnage::~Personnage()
 {
+//cout << "Destruction of the object" ;
 }
 
 int Personnage::getX() const
@@ -39,15 +46,42 @@ int Personnage::getD() const
 	return p_d;
 }
 
+int Personnage::getV() const
+{
+	return p_Vie;
+}
+
 void Personnage::setOrientation(int angle)
 {
 	if (angle > 0 && angle < 360)
 		p_rotation = angle;
 }
 
-void Personnage::marcher(int distance) {
-
+void Personnage::avancer(int distance) {
+	if (p_x+distance <= f_X_size)
+		p_x=p_x - distance;
+	
 }
+
+void Personnage::reculer(int distance) {
+	if (p_x+distance >= 0)
+		p_x=p_x - distance;
+	
+}
+
+void Personnage::monter(int distance) {
+	if (p_y+distance >= f_Y_size)
+		p_y=p_y + distance;
+	
+}
+
+void Personnage::descendre(int distance) {
+	if (p_y-distance >= 0)
+		p_y=p_y - distance;
+	
+}
+
+
 
 void Personnage::tirer(int nbDegats) {
 	if (p_vie > 0) {
